6161bc97b0c0e0335bcfde5273184c340532896b
yasoug <ougouisyassine@gmail.com>
2023-09-26 14:50:46 -0700

    task 5 : commit 2

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,18 +1,6 @@
-package { 'nginx':
-  ensure => installed,
-}
+# Configure nginx web server using Puppet
 
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-}
-
-file_line { 'Add redirection, 301':
-  path   => '/etc/nginx/sites-available/default',
-  ensure => 'present',
-  line   => '/redirect_me {\n return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}\n',
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+exec { 'install':
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; sudo bash -c "echo 'Hello World!' > /var/www/html/index.html" ; sudo sed -i "30i location /redirect_me {\n return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}\n" /etc/nginx/sites-available/default ; sudo service nginx restart',
+  provider => shell,
 }
