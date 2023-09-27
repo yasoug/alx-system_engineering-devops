d68c414484535ea778165e53ba05c41bc52e2ab6
yasoug <ougouisyassine@gmail.com>
2023-09-27 13:22:29 -0700

    task 5 : commit 2 (update)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,29 @@
 # Configure nginx web server using Puppet
 
-exec { 'install':
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; sudo bash -c "echo 'Hello World!' > /var/www/html/index.html" ; sudo sed -i "30i location /redirect_me {\n return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}\n" /etc/nginx/sites-available/default ; sudo service nginx restart',
-  provider => shell,
+exec { 'apt-update':
+  command     => 'apt-get -y update',
+  path        => '/usr/bin:/bin',
+  refreshonly => true,
+}
+exec { 'apt-get':
+  command => 'apt-get -y install nginx',
+  path    => '/usr/bin:/bin',
+  require => Exec['apt-update'],
+}
+
+-> file { '/var/www/html/index.nginx-debian.html':
+  content => 'Hello World!',
+}
+
+file_line { 'add_redirect':
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'server_name _;',
+  line   => '    location /redirect_me {return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;}',
+  notify => Exec['restart_nginx'],
+}
+
+exec { 'restart_nginx':
+  command     => '/usr/sbin/service nginx restart',
+  refreshonly => true,
+  subscribe   => File_line['add_redirect'],
 }
